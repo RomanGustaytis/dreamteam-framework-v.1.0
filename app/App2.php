620f875644b404d30385b84dccf92953231847c9
RomanGustaytis <roman.gustaytis@gmail.com>
2018-07-17 09:30:12 +0300

    Создал App2 класс и подключил его к роутеру.

---
+++ app/App2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 17.07.18
+ * Time: 9:28
+ */
+
+namespace app;
+
+
+use DreamTeam\Router;
+
+/**
+ * Class App2
+ * @package app
+ */
+class App2 extends Router
+{
+
+}

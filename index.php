8fb79614c8f8e56bf7fba641e45b422e09f37d58
RomanGustaytis <roman.gustaytis@gmail.com>
2018-07-17 10:38:05 +0300

    Создал App2 класс и подключил его к роутеру.

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 include_once('vendor/DreamTeam/Router.php');
+include_once('app/App2.php');
 
 use \DreamTeam\Router;
+use \app\App2;
 
-var_dump(Router::init()->getParams());
+
+
+var_dump(App2::init()->getParams());
